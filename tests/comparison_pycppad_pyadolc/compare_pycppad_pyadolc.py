b888a5fcf095654d3a798b1d3033718ee47af952
basti <basti@shlp.(none)>
2009-07-15 10:52:10 +0200

    added gradient comparison to pyadolc vs pycppad

--- tests/comparison_pycppad_pyadolc/compare_pycppad_pyadolc.py
+++ tests/comparison_pycppad_pyadolc/compare_pycppad_pyadolc.py
@@ -8,12 +8,14 @@ import adolc
 
 
 if __name__ == "__main__":
-	N_max = 200
-	reps = 30
+	N_max = 120
+	reps = 100
 
 	Ns = range(2,N_max,5)
-	adolc_runtimes = []
-	cppad_runtimes = []
+	adolc_gradient_runtimes = []
+	cppad_gradient_runtimes = []
+	adolc_hessian_runtimes = []
+	cppad_hessian_runtimes = []
 	
 	for N in Ns:
 	
@@ -29,8 +31,8 @@ if __name__ == "__main__":
 		x   = numpy.random.rand(N)
 		w   = numpy.array( [ 1.] ) # compute Hessian of x0 * sin(x1)
 		
-		cppad_runtime = timeit.Timer('f.hessian(x, w)', 'from __main__ import f,x,w').timeit(number=reps)/reps
-		
+		cppad_hessian_runtime  = timeit.Timer('f.hessian(x, w)', 'from __main__ import f,x,w').timeit(number=reps)/reps
+		cppad_gradient_runtime = timeit.Timer('f.jacobian(x)', 'from __main__ import f,x').timeit(number=reps)/reps
 		
 		# adolc timing
 		x     = numpy.zeros(N, dtype=float)
@@ -45,17 +47,26 @@ if __name__ == "__main__":
 		adolc.trace_off()
 		
 		x   = numpy.random.rand(N)
-		adolc_runtime = timeit.Timer('adolc.hessian(0, x)', 'import adolc;from __main__ import x').timeit(number=reps)/reps
-
-		adolc_runtimes.append(adolc_runtime)
-		cppad_runtimes.append(cppad_runtime)
+		adolc_hessian_runtime = timeit.Timer('adolc.hessian(0, x)', 'import adolc;from __main__ import x').timeit(number=reps)/reps
+		adolc_gradient_runtime = timeit.Timer('adolc.gradient(0, x)', 'import adolc;from __main__ import x').timeit(number=reps)/reps
+		
+		adolc_hessian_runtimes.append(adolc_hessian_runtime)
+		cppad_hessian_runtimes.append(cppad_hessian_runtime)
+		
+		adolc_gradient_runtimes.append(adolc_gradient_runtime)
+		cppad_gradient_runtimes.append(cppad_gradient_runtime)
 	
 	pylab.figure()
-	pylab.semilogy(Ns, adolc_runtimes, '-b.', label='pyadolc')
-	pylab.semilogy(Ns, cppad_runtimes, '-r.', label='pycppad')
+	pylab.semilogy(Ns, adolc_hessian_runtimes, '-b.', label='hessian pyadolc')
+	pylab.semilogy(Ns, adolc_gradient_runtimes, '-bd', label='gradient pyadolc')\
+	
+	pylab.semilogy(Ns, cppad_hessian_runtimes, '-r.', label='hessian pycppad')
+	pylab.semilogy(Ns, cppad_gradient_runtimes, '-rd', label='gradient pycppad')
+	
+	
 	pylab.xlabel(r'problem size $N$')
-	pylab.ylabel(r'runtime t [sec]')
-	pylab.title(r'Hessian computation')
-	pylab.legend()
+	pylab.ylabel(r'average runtime $t= \frac{1}{%d} \sum_{r=1}^{%d} t_r$  in [sec]'%(reps,reps))
+	pylab.title(r'Hessian/Gradient computation, %d runs '%reps)
+	pylab.legend(loc=2)
 	pylab.savefig('runtimes_pycppad_pyadolc.png')
 	pylab.show()
