5b52379d4b13eeb342d1df8cce34a63c7d35d1b8
basti <basti@shlp.(none)>
2009-08-02 19:14:02 +0200

    deleted the previously commented out old way of directly calling wrapper functions written in c++ from python: the prefered way is to use a thin wrapper on the python side that  does all the input checks and calls c++ with almost the same API as ADOL-C has (double *x is converted to boost::python::array though).

--- py_adolc.hpp
+++ py_adolc.hpp
@@ -53,29 +53,6 @@ extern adub ldexp ( const badouble&, int );
 void trace_on_default_argument(short tape_tag){ trace_on(tape_tag,0);}
 void trace_off_default_argument(){ trace_off(0);}
 
-// /* PYTHONIC CALLS OF FUNCTIONS */
-// bpn::array	wrapped_function			(short tape_tag, bpn::array &bpn_x);
-// bpn::array	wrapped_gradient			(short tape_tag, bpn::array &bpn_x);
-// bpn::array	wrapped_hessian				(short tape_tag, bpn::array &bpn_x);
-// bpn::array	wrapped_jacobian			(short tape_tag, bpn::array &bpn_x);
-// bpn::array	wrapped_vec_jac				(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_u, bool repeat);
-// bpn::array	wrapped_jac_vec				(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_v);
-// bpn::array	wrapped_hess_vec			(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_v);
-// bpn::array	wrapped_lagra_hess_vec		(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_u, bpn::array &bpn_v);
-// // void		wrapped_jac_solv			(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_b, int sparse, int mode);
-// bpn::array	wrapped_zos_forward			(short tape_tag, bpn::array &bpn_x, int keep);
-// bp::tuple	wrapped_fos_forward			(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_v, int keep);
-// bp::tuple 	wrapped_fov_forward			(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_V);
-// bp::tuple	wrapped_hos_forward			(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_V, int keep);
-// bp::tuple	wrapped_hov_forward			(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_V);
-// bp::tuple	wrapped_hov_wk_forward		(short tape_tag, bpn::array &bpn_x, bpn::array &bpn_V, int keep);
-// bpn::array wrapped_fos_reverse			(short tape_tag, bpn::array &bpn_u);
-// bpn::array wrapped_fov_reverse			(short tape_tag, bpn::array &bpn_U);
-// bpn::array wrapped_hos_reverse			(short tape_tag, int D, bpn::array &bpn_u);
-// bp::tuple wrapped_hov_reverse			(short tape_tag, int D, bpn::array &bpn_U);
-// bp::tuple wrapped_hov_ti_reverse		(short tape_tag, bpn::array &bpn_U);
-
-
 /* C STYLE CALLS OF FUNCTIONS */
 void c_wrapped_function			(short tape_tag, int M, int N, bpn::array &bpn_x, bpn::array &bpn_y );
 void c_wrapped_gradient			(short tape_tag, int N, bpn::array &bpn_x, bpn::array &bpn_g);
@@ -215,32 +192,6 @@ BOOST_PYTHON_MODULE(_adolc)
 	def("trace_on",trace_on_default_argument);
 	def("trace_off",trace_off_default_argument);
 
-// 	def("function", 		&wrapped_function);
-// 	def("gradient", 		&wrapped_gradient);
-// 	def("hessian",			&wrapped_hessian);
-// 	def("jacobian", 		&wrapped_jacobian);
-// 	def("vec_jac",			&wrapped_vec_jac);
-// 	def("jac_vec",			&wrapped_jac_vec);
-// 	def("hess_vec",			&wrapped_hess_vec);
-// 	def("lagra_hess_vec", 	&wrapped_lagra_hess_vec);
-
-// 	def("zos_forward",		&wrapped_zos_forward);
-// 	def("fos_forward",		&wrapped_fos_forward);
-// 	def("fov_forward",		&wrapped_fov_forward);
-// 	def("hos_forward",		&wrapped_hos_forward);
-// 	def("hov_forward",		&wrapped_hov_forward);
-
-// 	def("hov_wk_forward",	&wrapped_hov_wk_forward);
-													
-// 	def("fos_reverse",		&wrapped_fos_reverse);
-     
-// 	def("fov_reverse",		&wrapped_fov_reverse);
-													
-// 	def("hos_reverse",		&wrapped_hos_reverse);
-// 	def("hov_reverse", 		&wrapped_hov_reverse);
-
-// 	def("hov_ti_reverse", 	&wrapped_hov_ti_reverse);
-													
 
 	/* c style functions */
 	def("function", 		&c_wrapped_function);
@@ -395,8 +346,6 @@ BOOST_PYTHON_MODULE(_adolc)
 	;
 
 	class_<adub, bases<badouble> >("adub", no_init)
-// 			.def(init<locint>())
-// 			.def(init<const adub &>())
 	;
 
 	class_<adouble, bases<badouble> >("adouble", init<double>())
